e0b473624b8d44f05b31c1b8bceff51eee1961a6
Karim Al-Savadi <alsavidi37@gmail.com>
2022-01-30 19:49:45 +0300

    adding methods for state changing

--- mini-octopus-core/src/main/scala/core/creatures/BasicCreature.scala
+++ mini-octopus-core/src/main/scala/core/creatures/BasicCreature.scala
@@ -6,5 +6,17 @@ trait Creature {
   var hp: Int
   var maxHp: Int
   var attack: Int
-  var state: Status
+  var status: Status
+
+  def getDamaged(damage: Int): Unit = hp -= damage
+
+  def getHealed(heal: Int): Unit = heal + hp match {
+    case healedHp if healedHp > maxHp => hp = maxHp
+    case healedHp => hp = healedHp 
+  }
+
+  def getActualStatus = hp match {
+    case hp if hp <= 0 => { status = Status.Dead; status }
+    case _ => status 
+  }
 }
