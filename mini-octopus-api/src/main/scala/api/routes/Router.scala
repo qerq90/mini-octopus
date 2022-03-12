a46bf58274d0387b099cc868e18b758258c0e4b6
Karim Al-Savadi <alsavidi37@gmail.com>
2022-03-13 01:13:08 +0300

    added message example structure

--- mini-octopus-api/src/main/scala/api/routes/Router.scala
+++ mini-octopus-api/src/main/scala/api/routes/Router.scala
@@ -25,3 +25,32 @@ object Router {
         UIO(Response.text("Ok"))
     }
 }
+
+/* {
+                "type":"message_new",
+                "object":{
+                  "message":{
+                    "date":1647049000,
+                    "from_id":51422811,
+                    "id":3774,
+                    "out":0,
+                    "attachments":[],
+                    "conversation_message_id":2551,
+                    "fwd_messages":[],
+                    "important":false,
+                    "is_hidden":false,
+      !!!!!         "peer_id":51422811,
+                    "random_id":0,
+      !!!!!         "text":"1231"
+                   },
+                   "client_info":{
+                    "button_actions":["text","vkpay","open_app","location","open_link","callback","intent_subscribe","intent_unsubscribe"],
+                    "keyboard":true,
+                    "inline_keyboard":true,
+                    "carousel":true,
+                    "lang_id":0
+                   }
+                  },
+                  "group_id":171104414,
+                  "event_id":"45586712573ba1939fbad3b0f37ed885781bb8a6"
+                 }*/
